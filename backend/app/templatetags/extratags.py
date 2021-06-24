b53ac4c9ef85de6a7a5b031ae867f292b74506c7
Jesvin Jose <frank.einstien@gmail.com>
2021-06-24 16:39:19 +0530

    Updated image of Opengraph 2

--- backend/app/templatetags/extratags.py
+++ backend/app/templatetags/extratags.py
@@ -4,6 +4,7 @@ from django import template
 from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import QueryDict
+from django.http.request import HttpRequest
 from django.utils.http import urlencode
 from django.utils.safestring import mark_safe
 
@@ -13,6 +14,10 @@ register = template.Library()
 @register.simple_tag
 def hcaptcha_sitekey():
     return settings.HCAPTCHA_SITEKEY
+
+@register.filter
+def base_url(request: HttpRequest):
+    return request._current_scheme_host
 
 
 # todo remove
@@ -36,3 +41,4 @@ def none_for_1(value):
 def url_with_args(value: QueryDict, **kwargs):
     shit = {k: v for k, v in {**value.dict(), **kwargs}.items() if v}
     return "?" + urlencode(shit)
+
